44dd1e747d144b8f7298c33c7cab5df980bb606c
Bmgpt <137195478+Bmgpt@users.noreply.github.com>
2024-09-13 17:24:18 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -39,8 +39,4 @@ SAFETY_SETTTINGS = [
         "category": "HARM_CATEGORY_MEDICAL",
         "threshold": "BLOCK_NONE",
     },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS",
-        "threshold": "BLOCK_NONE",
-    },
 ]
